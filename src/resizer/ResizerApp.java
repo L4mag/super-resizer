1042cd61803151ec5e17ca33422c00e8ce8ec7d0
gheryesss <gheryesss@gmail.com>
2021-05-29 20:33:57 +0300

    готово

---
+++ src/resizer/ResizerApp.java
@@ -0,0 +1,28 @@
+package mts.teta.resizer;
+
+import mts.teta.resizer.imageprocessor.ImageProcessor;
+import picocli.CommandLine;
+
+import javax.imageio.ImageIO;
+import java.util.concurrent.Callable;
+
+public class ResizerApp extends ConsoleAttributes implements Callable<Integer> {
+    public static void main(String... args) {
+        int exitCode = runConsole(args);
+        System.exit(exitCode);
+    }
+
+    protected static int runConsole(String... args) {
+        return new CommandLine(new ResizerApp()).execute(args);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        ImageProcessor imageProcessor = new ImageProcessor();
+
+        imageProcessor.processImage(ImageIO.read(inputFile), this);
+
+        System.out.println("run");
+        return 0;
+    }
+}
